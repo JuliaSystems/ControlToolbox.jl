9f2b5444e57dcfad6f7277d931673cc5423b112a
Cristian Rojas <crro@kth.se>
2016-10-26 13:39:21 +0200

    Modified isstable.jl for new data types

--- src/analysis/isstable.jl
+++ src/analysis/isstable.jl
@@ -1,7 +1,13 @@
 
-isstable{T<:ControlCore.DSiso}(s::T) = maximum(abs([poles(s); 0])) < 1
+isstable{S}(s::LtiSystem{S,Continuous{false}}) = maximum(abs([poles(s); 0])) < 1
 
-isstable{T<:ControlCore.CSiso}(s::T) = maximum(real([poles(s); 0])) < 0
+isstable{S}(s::LtiSystem{S,Continuous{true}}) = maximum(real([poles(s); 0])) < 0
 
-isstable{T<:ControlCore.MimoSystem}(s::T) = map(isstable, getmatrix(s))
+isstable(s::LtiSystem{Siso{false}}) = map(isstable, getmatrix(s))
+
 # TODO: How to handle pole-zero cancellation
+
+# Legacy
+#isstable{T<:ControlCore.DSiso}(s::T) = maximum(abs([poles(s); 0])) < 1
+#isstable{T<:ControlCore.CSiso}(s::T) = maximum(real([poles(s); 0])) < 0
+#isstable{T<:ControlCore.MimoSystem}(s::T) = map(isstable, getmatrix(s))
