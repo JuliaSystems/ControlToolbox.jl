f9b08b8848b56cebfcc6e96031c535748ed096b1
Cristian Rojas <crro@kth.se>
2016-10-26 23:09:53 +0200

    Added d2c

--- src/ControlToolbox.jl
+++ src/ControlToolbox.jl
@@ -10,12 +10,13 @@ export c2d, lsim, step, impulse, isstable, rlocus
 # using DSP
 
 include("c2d.jl")
+include("d2c.jl")
 #include("simulation/utils.jl")
 #include("simulation/lsim.jl")
 #include("simulation/step.jl")
 #include("simulation/impulse.jl")
-#include("analysis/isstable.jl")
+include("analysis/isstable.jl")
 #include("analysis/rlocus.jl")
-#include("analysis/dcgain.jl")
+include("analysis/dcgain.jl")
 
 end # module
---
+++ src/d2c.jl
@@ -0,0 +1,25 @@
+"""
+    d2c(s)
+
+Recovers the continuous time which was sampled with a zero-order-hold to obtain
+the discrete time system `s`.
+"""
+function d2c{S}(s::StateSpace{S,Continuous{false}})
+  A, B, C, D = s.A, s.B, s.C, s.D
+  for λ in eig(A)[1]
+    println(λ)
+    if λ < zero(λ)
+      error("d2c cannot be applied to systems with negative eigenvalues")
+    end
+  end
+  ny, nu = size(s)
+  nx = s.nx
+  M = logm([A  B; zeros(nu, nx) eye(nu)]) / s.Ts
+  Ac = M[1:nx, 1:nx]
+  Bc = M[1:nx, nx+1:nx+nu]
+  Cc = C
+  Dc = D
+  ss(Ac, Bc, Cc, Dc)
+end
+
+d2c{S}(s::LtiSystem{S,Continuous{false}}) = d2c(ss(s))
