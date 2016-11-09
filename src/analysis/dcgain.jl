894633e42c65bdc7eba4b020548669ba9ccc0846
Cristian Rojas <crro@kth.se>
2016-11-09 06:21:49 +0100

    Shortened dcgain

--- src/analysis/dcgain.jl
+++ src/analysis/dcgain.jl
@@ -1,12 +1,6 @@
-dcgain(s::LtiSystem) = dcgain(ss(s))
-
-function _dcgain{S}(s::StateSpace{S,Continuous{true}})
-  return -s.C/s.A*s.B + s.D
-end
-
-function _dcgain{S}(s::StateSpace{S,Continuous{false}})
-  return s.C/(speye(size(s.A)) - s.A)*s.B + s.D
-end
-
 dcgain(s::StateSpace{Siso{true}}) = _dcgain(s)[1]
 dcgain(s::StateSpace{Siso{false}}) = _dcgain(s)
+dcgain(s::LtiSystem) = dcgain(ss(s))
+
+_dcgain{S}(s::StateSpace{S,Continuous{true}}) = s.D - s.C/s.A*s.B
+_dcgain{S}(s::StateSpace{S,Continuous{false}}) = s.C/(speye(size(s.A)) - s.A)*s.B + s.D
